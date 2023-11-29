1e3f074e01db277e8389540631997d6b10649382
Gowsalya Sanjai <145602948+gowsiraj@users.noreply.github.com>
2023-11-29 14:35:14 +0400

    Update us.js

--- us.js
+++ us.js
@@ -1,18 +1,15 @@
 
 //Print the country that uses US dollars as currency.
 var request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.eu/rest/v2/all', true)
+request.open("GET" ,"https://restcountries.com/v3.1/all");
 request.send();
-
-request.onload = function () {
-    var data = JSON.parse(this.response);
-
-   
-    var usDollarCountries = data.filter(country => country.currencies[0].code=== "$");
-
-  
-    usDollarCountries.forEach(country => {
-        console.log( country.name,country.currencies[0].code);
-    });
+request.onload  = function(){
+      var result=JSON.parse(request.response);
+      var currency = result.filter((ele)=>ele.currencies);
+      console.log(currency);
+      var money = currency.map((elem)=>elem.currencies);
+      console.log(money);
+      var doller = cur.filter((element)=>element.USD);
+      console.log(doller); 
 }
 
